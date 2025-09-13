9fee880fa0a7582123be7faadd886636b7b01129
MohamadLatifi <goofySmart8@gmail.com>
2025-09-13 17:47:07 +0430

    first script

---
+++ script.js
@@ -0,0 +1,34 @@
+const Glass = document.querySelectorAll(".glass")
+const Water = document.querySelectorAll(".glass .water") 
+const bottle = document.querySelector(".bottle")
+
+
+
+let bottleHeight = 0;
+const savedElement = [];
+Glass.forEach((glass, i) => {
+
+
+    glass.addEventListener("click", () => {
+
+
+        Water.forEach((water, j) => {
+
+            if (i === j) {
+
+                water.style.transition = "all 0.9s ease-in";
+                water.style.height = "100%";
+                bottleHeight += 12.5
+                bottle.children[0].style.height = bottleHeight + "%"
+
+
+                localStorage.setItem("elemntHeights", JSON.stringify(savedElement))
+
+            }
+
+        })
+
+    })
+})
+
+
