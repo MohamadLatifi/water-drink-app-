bf1e943e8e5fd2cd5bab99830938e513071a0aec
MohamadLatifi <goofySmart8@gmail.com>
2025-10-10 16:36:03 +0330

    fix the glass function first attempt

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-const Glass = document.querySelectorAll(".glass")
-const Water = document.querySelectorAll(".glass .water")
-const bottle = document.querySelector(".bottle")
-
-
+const Glass = document.querySelectorAll(".glass");
+const Water = document.querySelectorAll(".glass .water");
+const bottle = document.querySelector(".bottle");
 
 let bottleHeight = 0;
-const savedElement = [];
+
 Glass.forEach((glass, i) => {
-
-
+    let isFilde=false;
     glass.addEventListener("click", () => {
-
-
+        
         Water.forEach((water, j) => {
+            
+            if (i === j && bottleHeight < 100 && isFilde==false ) {
+                water.style.transition = "all 0.9s ease-in";
+                water.style.height = "100%";
+                bottleHeight += 12.5;
+                bottle.children[0].style.height = bottleHeight + "%";
+                isFilde = true;
 
-
-
-
-                if (i === j && bottleHeight<100) {
-
-                    water.style.transition = "all 0.9s ease-in";
-                    water.style.height = "100%";
-                    bottleHeight += 12.5
-                    bottle.children[0].style.height = bottleHeight + "%"
-                    
-                }   
-
-
-                // localStorage.setItem("elemntHeights", JSON.stringify(savedElement))
-
-
+            }
 
         })
 
-    })
+    }
+    )
 })
 
+                // if (i === j && bottleHeight < 100 || i === false) {
+                //     console.log("dubbelclick");
+                //     water.style.transition = "all 0.9s ease-in";
+                //     water.style.height = "0%";
+                //     bottleHeight += -12.5
+                //     bottle.children[0].style.height = bottleHeight + "%"
 
+                // }
