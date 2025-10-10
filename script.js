0398553977b0d4e075075c3e7ba1fd7693c2dcfb
MohamadLatifi <goofySmart8@gmail.com>
2025-10-10 17:19:13 +0330

    local storge for bottle

--- script.js
+++ script.js
@@ -2,34 +2,37 @@ const Glass = document.querySelectorAll(".glass");
 const Water = document.querySelectorAll(".glass .water");
 const bottle = document.querySelector(".bottle");
 
-let bottleHeight = 0;
+let bottleHeight = JSON.parse(localStorage.getItem("bottleHeight")) || 0;
+// let glassStates = JSON.parse(localStorage.getItem("glassStates")) || [];
+bottle.children[0].style.height = bottleHeight + "%";
 
 Glass.forEach((glass, i) => {
-    let isFilde=false;
+    let isFilled = false;
     glass.addEventListener("click", () => {
-        
         Water.forEach((water, j) => {
-            
-            if (i === j && bottleHeight < 100 && isFilde==false ) {
+            if (i === j && bottleHeight < 100 && isFilled == false) {
                 water.style.transition = "all 0.9s ease-in";
                 water.style.height = "100%";
                 bottleHeight += 12.5;
                 bottle.children[0].style.height = bottleHeight + "%";
-                isFilde = true;
+                isFilled = true;
 
+                localStorage.setItem("bottleHeight", JSON.stringify(bottleHeight))
             }
+        });
+    });
+});
 
-        })
 
-    }
-    )
-})
 
-                // if (i === j && bottleHeight < 100 || i === false) {
-                //     console.log("dubbelclick");
-                //     water.style.transition = "all 0.9s ease-in";
-                //     water.style.height = "0%";
-                //     bottleHeight += -12.5
-                //     bottle.children[0].style.height = bottleHeight + "%"
+// if (i === j && bottleHeight < 100 || i === false) {
+//     console.log("dubbelclick");
+//     water.style.transition = "all 0.9s ease-in";
+//     water.style.height = "0%";
+//     bottleHeight += -12.5
+//     bottle.children[0].style.height = bottleHeight + "%"
 
-                // }
+// }
+
+
+
